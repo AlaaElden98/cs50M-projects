53d524a7f92ce0e33fd0434cf8503d50952c2536
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-02-09 12:41:59 +0200

    build Timer screen and TimerCountDown component

---
+++ projects/project1/src/components/timerCountDown/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+export const TimerCountDown = (props) => {
+  return (
+    <View>
+      <Text>00:00</Text>
+    </View>
+  );
+};
---
+++ projects/project1/src/screens/Timer/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { TimerCountDown } from "../../components/timerCountDown";
+
+export const Timer = (props) => {
+  return <TimerCountDown />;
+};
